163be7333cb33ecadd5670ac396fd122bb8f52f8
Billy Hadlow <me@billyhadlow.com>
2025-09-18 05:50:52 -0700

    tailwind upgrade

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,41 +1,45 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
-
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://sendex.dev',
-	integrations: [starlight({
-		title: 'Sendex Docs',
-		logo: {
-			src: './src/assets/logo.svg',
-			replacesTitle: true,
-		},
-		social: {
-			github: 'https://github.com/hadlow/sendex',
-		},
-		sidebar: [
-			{
-				label: 'Getting started',
-				items: [
-					{ label: 'Introduction', slug: 'getting-started/intro' },
-					{ label: 'Installation', slug: 'getting-started/installation' },
-					{ label: 'Basic usage', slug: 'getting-started/basic-usage' },
-				],
+	integrations: [
+		starlight({
+			title: 'Sendex Docs',
+			logo: {
+				src: './src/assets/logo.svg',
+				replacesTitle: true,
 			},
-			{
-				label: 'Learn more',
-				items: [
-					{ label: 'Outputs', slug: 'learn-more/outputs' },
-					{ label: 'Environment variables', slug: 'learn-more/environment-variables' },
-					{ label: 'Request configuration', slug: 'learn-more/request-configuration' },
-					{ label: 'Optional flags', slug: 'learn-more/optional-flags' },
-					{ label: 'End-to-end testing', slug: 'learn-more/testing' },
-					{ label: 'Support', slug: 'learn-more/support' },
-				],
-			},
-		],
-	}), tailwind()],
+			// social: {
+			// 	github: 'https://github.com/hadlow/sendex',
+			// },
+			sidebar: [
+				{
+					label: 'Getting started',
+					items: [
+						{ label: 'Introduction', slug: 'getting-started/intro' },
+						{ label: 'Installation', slug: 'getting-started/installation' },
+						{ label: 'Basic usage', slug: 'getting-started/basic-usage' },
+					],
+				},
+				{
+					label: 'Learn more',
+					items: [
+						{ label: 'Outputs', slug: 'learn-more/outputs' },
+						{ label: 'Environment variables', slug: 'learn-more/environment-variables' },
+						{ label: 'Request configuration', slug: 'learn-more/request-configuration' },
+						{ label: 'Optional flags', slug: 'learn-more/optional-flags' },
+						{ label: 'End-to-end testing', slug: 'learn-more/testing' },
+						{ label: 'Support', slug: 'learn-more/support' },
+					],
+				},
+			],
+		}),
+	],
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
